b6f1913a16e6793fb7ec8377e619fdb8c86924e6
Olga Malkovich <olga.malkovich@gmail.com>
2022-05-02 20:02:45 +0300

    fix: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -63,9 +63,41 @@ class BinarySearchTree {
     return node;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootNode = removeNode(this.rootNode, data);
+
+    function removeNode (node, data) {
+      if (!node) {
+        return null;
+      }
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && node.right) {
+          return null;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+        let rightMin = node.right;
+        while (rightMin.left) {
+          rightMin = rightMin.left;
+        }
+        node.data = rightMin.data;
+        node.right = removeNode(node.right, rightMin.data);
+
+        return node;
+      }
+    }
   }
 
   min() {
